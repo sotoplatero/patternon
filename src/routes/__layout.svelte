a2b402ccb3fadc5fcbb97a75962e4a280a4d20ee
Damian Soto <soto.platero@gmail.com>
2021-09-02 08:11:38 -0400

    finishing

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,11 +11,11 @@
 
 	<slot />
 
-  <footer class="mt-24 mb-12 text-sm text-center font-mono space-y-6">
+  <footer class="max-w-screen-sm mx-auto mt-24 mb-12 text-sm text-center font-mono space-y-6">
   	<div>Hecho con <a href="https://kit.svelte.dev" target="_blank" rel="nofollow noopener">SvelteKit</a>, <a href="https://tailwindcss.com" target="_blank" rel="nofollow noopener">Tailwind</a> y <a href="https://www.heropatterns.com/">heropatterns</a></div>
 
   	<div>
-      Si te 💓 PresentURL prueba <a href="htps://cssfinder">CSSFinde</a> y sigueme en <a href="https://twitter.com/sotoplatero" class="text-blue-500" target="_blank" rel="me">@sotoplatero</a> para más proyectos e ideas locas.
+      Si te 💓 PresentURL prueba <a href="htps://cssfinder.xyz">CSSFinder</a> y sigueme en <a href="https://twitter.com/sotoplatero" class="text-blue-500" target="_blank" rel="me">@sotoplatero</a> para más proyectos e ideas locas.
     </div>
   </footer>
 
